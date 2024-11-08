3c7c8f984ad5593c2eec0ab5c904b7d5d76855fc
kasrasadeghi <kasra.sadeghi1@gmail.com>
2024-11-07 23:12:31 -0800

    number tests for recommended implementation order

---
+++ examples/06_return_if.c
@@ -0,0 +1,7 @@
+int main(int argc, char *argv[]) {
+  if (argc > 2) {
+    return 10;
+  } else {
+    return 20;
+  }
+}
---
+++ examples/06_return_if.py
@@ -0,0 +1,5 @@
+def main(argc, argv):
+  if argc > 2:
+    return 10
+  else:
+    return 20
